917eab9713d9c0ce03b4fad2000e7d70166f7282
yunkai-zhang <59794765+yunkai-zhang@users.noreply.github.com>
2022-04-06 00:08:13 +0800

    更新

--- src/main/java/com/zhangyun/zseckill/controller/UserController.java
+++ src/main/java/com/zhangyun/zseckill/controller/UserController.java
@@ -1,8 +1,11 @@
 package com.zhangyun.zseckill.controller;
 
 
+import com.zhangyun.zseckill.pojo.User;
+import com.zhangyun.zseckill.vo.RespBean;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.ResponseBody;
 
 /**
  * <p>
@@ -15,5 +18,12 @@ import org.springframework.web.bind.annotation.RequestMapping;
 @Controller
 @RequestMapping("/user")
 public class UserController {
+
+    //查看当前登录的用户的信息
+    @RequestMapping("/info")
+    @ResponseBody
+    public RespBean info (User user){
+        return RespBean.success(user);
+    }
 
 }
